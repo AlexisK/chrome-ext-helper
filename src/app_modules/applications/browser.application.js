fca06dec097d524f6602f003581973628bfb6ee5
oleksiikaliuzhnyi <oleksii.kaliuzhnyi@qvantel.com>
2018-02-13 17:00:47 +0200

    notes update

--- src/app_modules/applications/browser.application.js
+++ src/app_modules/applications/browser.application.js
@@ -9,25 +9,34 @@ export const BrowserApplication = new Application({
     },
     isWide: true,
     createView: function() {
-        this.lastLink = this.data.link;
-
         this.rootNode = new DomEl('div').cls('app-application-browser');
         this.pathNode = this.rootNode.cr('input').attr({type:'text'}).value(this.data.link);
         this.iframeWrap = this.rootNode.cr('div').cls('iframe-wrap');
         this.iframe = this.iframeWrap.cr('iframe').attr({src: this.data.link});
 
-        this.pathNode.addEventListener('keyup', ev => {
-            if ( ev.keyCode === 13 ) { // Enter
-                this.data.link = this.pathNode.getValue();
-                this.save();
+        chrome.extension.onMessage.addListener(request => {
+            let url = request['iframe-url-hook'];
+            if ( url ) {
+                let urlParams = url.split('/');
+                if ( urlParams[2] && urlParams[2] === this.data.domain ) {
+                    this.data.link = url;
+                    this.pathNode.value(url);
+                    this.save();
+                }
             }
         });
 
-        this.ev.subscribe('data', () => {
-            if ( this.data.link !== this.lastLink ) {
-                this.iframe.attr({src: (this.data.link)});
+        this.setUpNewPath = url => {
+            this.data.link = url;
+            this.data.domain = url.split('/')[2];
+            this.save();
+            this.iframe.attr({src: url});
+        };
+
+        this.pathNode.addEventListener('keyup', ev => {
+            if ( ev.keyCode === 13 ) { // Enter
+                this.setUpNewPath(this.pathNode.getValue());
             }
-            this.lastLink = this.data.link;
         });
 
         return this.rootNode;
---
+++ src/static/iframe-url-hook.js
@@ -0,0 +1,1 @@
+chrome.extension.sendMessage({'iframe-url-hook': window.location.href});
