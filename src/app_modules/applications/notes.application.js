36665e28ad70f1ce284f8820ec3910c6c770a00b
oleksiikaliuzhnyi <oleksii.kaliuzhnyi@qvantel.com>
2018-02-14 08:39:51 +0200

    notes update

--- src/app_modules/applications/notes.application.js
+++ src/app_modules/applications/notes.application.js
@@ -19,138 +19,155 @@ export const NotesApplication = new Application({
     createView: function() {
         this.isInEditState = false;
         this.rootNode = new DomEl('div').cls('app-application-notes');
-
-        this.editButton = this.rootNode
-            .cr('button')
-            .addEventListener('click', () => {
-                this.rootContent.value('');
-                this.isInEditState = !this.isInEditState;
-                this.renderData(this.data.content);
-            })
-            .cr('img').attr({src: 'svg/edit.svg'});
-
         this.rootContent = this.rootNode.cr('div').cls('content');
 
-        this.drawInput = (parent, value) => {
-            let wrapNode = parent.cr('div').cls('input');
 
-            if ( this.isInEditState ) {
-                return wrapNode.cr('input').attr({type: 'text'}).value(value);
+        // render
+        this.renderField = (data) => {
+            if ( /^https?:\/\//.exec(data) ) {
+                return new DomEl('a').value(data).attr({href: data, target: 'blank'});
+            } else {
+                return new DomEl('div').value(data);
             }
-            let values = value.split(/\s*:\s+/);
-            if ( values.length === 1) {
-                return wrapNode.cr('div').value(value);
-            }
-            values[1] = values.slice(1).join(': ');
-            wrapNode.cr('div').cls('key').value(values[0]);
-            if ( /^https?:\/\//.exec(values[1]) ) {
-                return wrapNode.cr('a').value(values[1]).attr({href: values[1], target: 'blank'});
-            }
-            return wrapNode.cr('div').value(values[1]);
         };
 
-        this.renderData = (dataList = [], parentNode = this.rootContent) => {
-            if ( this.isInEditState ) {
-                parentNode
-                    .cr('div').cls('input')
-                    .cr('input').attr({type: 'text', placeholder: 'new group...'})
-                    .addEventListener('keyup', ev => {
-                        if ( ev.keyCode === 13 ) {
-                            dataList.push({
-                                title: ev.target.value,
-                                isOpened: true,
-                                content: []
-                            });
-                            this.save();
-                        }
-                    });
-            }
-            dataList.forEach((elem, ind) => {
-                if ( typeof(elem) === 'object') {
-                    elem.content = elem.content || [];
+        this.renderDirectory = (elem, parent = this.rootContent, parentElem = {}) => {
+            elem.content = elem.content || [];
+            let wrapNode = parent.cr('div').cls('directory ' + (elem.isEditing && 'edit-mode' || 'view-mode'));
+            let directories = elem.content.filter(v => (typeof(v) === 'object'));
 
-                    let titleBar = parentNode.cr('div').cls('group-title');
-
-                    titleBar
-                        .cr('button')
-                        .addEventListener('mousedown', () => {
-                            elem.isOpened = !elem.isOpened;
-                            this.save();
-                        })
-                        .cr('img').attr({src: elem.isOpened && 'svg/folder-10.svg' || 'svg/folder-5.svg'});
-
-                    if ( this.isInEditState ) {
-                        titleBar
-                            .cr('button')
-                            .addEventListener('click', () => {
-                                modalService.confirm('Delete '+elem.title+'?').then(() => {
-                                    dataList.splice(ind, 1);
-                                    this.save();
-                                });
-                            })
-                            .cr('img').attr({src: 'svg/garbage.svg'});
-                        titleBar
-                            .cr('button')
-                            .addEventListener('click', () => {
-                                if ( ind > 0 ) {
-                                    dataList.splice(ind, 1);
-                                    dataList.splice(ind-1, 0, elem);
-                                    this.save();
-                                }
-                            })
-                            .cr('img').attr({src: 'svg/arr-top.svg'});
-                        titleBar
-                            .cr('button')
-                            .addEventListener('click', () => {
-                                if ( ind < dataList.length - 1 ) {
-                                    dataList.splice(ind, 1);
-                                    dataList.splice(ind+1, 0, elem);
-                                    this.save();
-                                }
-                            })
-                            .cr('img').attr({src: 'svg/arr-bottom.svg'});
-                    }
-                    titleBar.cr('strong').value(elem.title);
-                    if ( elem.isOpened ) {
-                        this.renderData(elem.content, parentNode.cr('div').cls('group'));
-                    }
-                } else {
-                    let inp = this.drawInput(parentNode, elem);
-                    if ( this.isInEditState ) {
-                        inp.addEventListener('keyup', ev => {
-                            ev.target.classList.add('dirty');
-                            if (ev.keyCode === 13) {
-                                let val = ev.target.value;
-                                if (val.length === 0) {
-                                    dataList.splice(ind, 1);
-                                } else {
-                                    dataList[ind] = val;
-                                }
+            // title
+            let titleBar = wrapNode.cr('div').cls('group-title');
+            titleBar
+                .cr('button')
+                .cr('img').attr({src: elem.isOpened && 'svg/folder-10.svg' || 'svg/folder-5.svg'});
+            titleBar
+                .cr('button').cls('right')
+                .addEventListener('mousedown', ev => {
+                    ev.preventDefault();
+                    elem.isEditing = !elem.isEditing;
+                    this.save();
+                })
+                .cr('img').attr({src: elem.isEditing && 'svg/locked-1.svg' || 'svg/locked.svg'});
+            if ( parentElem.isEditing ) {
+                titleBar
+                    .cr('button').cls('right')
+                    .addEventListener('click', ev => {
+                        ev.preventDefault();
+                        modalService.confirm('Delete '+elem.title+'?').then(() => {
+                            let ind = parentElem.content.indexOf(elem);
+                            if ( ind >= 0 ) {
+                                parentElem.content.splice(ind, 1);
                                 this.save();
                             }
                         });
-                    }
-                }
-            });
-            if ( this.isInEditState ) {
-                parentNode
-                    .cr('div').cls('input')
-                    .cr('input').attr({type: 'text', placeholder: 'new record...'})
-                    .addEventListener('keyup', ev => {
-                        if ( ev.keyCode === 13 ) {
-                            dataList.push(ev.target.value);
+                    })
+                    .cr('img').attr({src: 'svg/garbage.svg'});
+                titleBar
+                    .cr('button').cls('right')
+                    .addEventListener('click', ev => {
+                        ev.preventDefault();
+                        let ind = parentElem.content.indexOf(elem);
+                        if ( ind > 0 ) {
+                            parentElem.content.splice(ind, 1);
+                            parentElem.content.splice(ind-1, 0, elem);
                             this.save();
                         }
-                    });
+                    })
+                    .cr('img').attr({src: 'svg/arr-top.svg'});
+                titleBar
+                    .cr('button').cls('right')
+                    .addEventListener('click', ev => {
+                        ev.preventDefault();
+                        let ind = parentElem.content.indexOf(elem);
+                        if ( ind >= 0 && ind < parentElem.content.length-1 ) {
+                            parentElem.content.splice(ind, 1);
+                            parentElem.content.splice(ind+1, 0, elem);
+                            this.save();
+                        }
+                    })
+                    .cr('img').attr({src: 'svg/arr-bottom.svg'});
             }
+            titleBar.cr('strong')
+                .addEventListener('mousedown', () => {
+                    elem.isOpened = !elem.isOpened;
+                    this.save();
+                })
+                .value(elem.title || 'ROOT');
+
+
+            // body
+            if ( elem.isOpened ) {
+                let groupNode = wrapNode.cr('div').cls('group');
+                directories.forEach(d => this.renderDirectory(d, groupNode, elem));
+                let lines = elem.content.filter(v => (typeof(v) === 'string'));
+
+                if ( elem.isEditing ) {
+                    groupNode
+                        .cr('div').cls('input')
+                        .cr('input').attr({type: 'text', placeholder: 'new group...'})
+                        .addEventListener('keyup', ev => {
+                            if ( ev.keyCode === 13 ) {
+                                elem.content.push({
+                                    title: ev.target.value,
+                                    isOpened: true,
+                                    isEditing: true,
+                                    content: []
+                                });
+                                this.save();
+                            }
+                        });
+                    let textarea = groupNode
+                        .cr('div').cls('input')
+                        .cr('textarea').attr({placeholder: 'text here...'})
+                        .value(lines.join('\n'))
+                        .addEventListener('blur', ev => {
+                            elem.content = [...directories, ...ev.target.value.split('\n')];
+                            this.save();
+                        })
+                        .addEventListener('keyup', ev => {
+                            let node = ev.target;
+                            node.style.minHeight = node.scrollHeight + 'px';
+                        });
+                    setTimeout(() => textarea.node.style.minHeight = textarea.node.scrollHeight + 'px', 1);
+
+                } else {
+                    lines.forEach(str => {
+                        if ( str.length ) {
+                            let node = groupNode.cr('div').cls('input');
+                            let values = str.split(/\s*:\s+/);
+                            if ( values.length === 1) {
+                                node.cr('div').value(str);
+                            } else {
+                                node.cls('input key-val');
+
+                                for ( let i = 0, len = values.length-1; i < len; i++ ) {
+                                    this.renderField(values[i]).cls('key').attachTo(node);
+                                }
+                                this.renderField(values[values.length-1]).attachTo(node);
+
+                                // values[1] = values.slice(1).join(': ');
+                                // node.cr('div').cls('key').value(values[0]);
+                                // if ( /^https?:\/\//.exec(values[1]) ) {
+                                //     node.cr('a').value(values[1]).attr({href: values[1], target: 'blank'});
+                                // } else {
+                                //     node.cr('div').value(values[1]);
+                                // }
+                            }
+                        }
+                    });
+                }
+            }
+
+
         };
 
         this.ev.subscribe('data', () => {
             this.rootContent.value('');
-            this.renderData(this.data.content);
+            this.renderDirectory(this.data);
         });
 
-        this.renderData(this.data.content);
+        this.renderDirectory(this.data);
 
         return this.rootNode;
     }
